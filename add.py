49c82f11a7f6a9321ed7e20f42d0bb9c63596a76
DoJale007 <israeljale98@gmail.com>
2025-08-14 09:47:54 +0000

    functions and modules

---
+++ add.py
@@ -0,0 +1,4 @@
+def add_task():
+    # Save task to database
+    # Return True
+    return True
---
+++ taskmng_com.py
@@ -0,0 +1,13 @@
+# # Define a register user function
+# def register_user(name, email, password):
+#     # Check if user does not already exist
+#     # Hash user password
+#     # Validate input
+#     # Check if user is not a robot
+#     # Return response
+#     return "User registered successfully!"
+
+# response = register_user("Israel Jale", "dojale007@yahoo.com", "0571510126")
+
+# # Calling the register user function
+# print(response)
