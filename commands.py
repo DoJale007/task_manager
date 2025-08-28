894cd0bc8bb0792711c0adda27b17a510ef6ff8e
DoJale007 <israeljale98@gmail.com>
2025-08-28 11:42:02 +0000

    Included Edit and Update Functionalities to Task Manager

--- commands.py
+++ commands.py
@@ -22,7 +22,8 @@ def get_tasks():
     return all_tasks
 
 
-def update_task(task, update):
+def update_task(id, update):
     # Update task in database
+    db.tasks.update_one({"_id": id}, {"$set": update})
     # Return response
     return True
